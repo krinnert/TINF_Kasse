9af4324e54ef6e6dadae290709c96fdbff584f0e
wirthual <wirthra@gmail.com>
2013-04-05 13:07:21 +0200

    Test

--- SmartKasse/src/com/example/smartkasse/MainActivity.java
+++ SmartKasse/src/com/example/smartkasse/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.smartkasse;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.slidingmenu.lib.SlidingMenu;
 import com.slidingmenu.lib.app.SlidingActivity;
 
@@ -14,13 +16,16 @@ public class MainActivity extends SlidingActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         setBehindContentView(R.layout.behind_layout);
+        
+        
         
         SlidingMenu slidingMenu = getSlidingMenu();
         ActionBar actionBar = getSupportActionBar();
         
         //SlidingMenu einrichten
-        slidingMenu.setBehindOffset(150);
+        slidingMenu.setBehindWidth(330);
         slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
         slidingMenu.setTouchModeBehind(SlidingMenu.TOUCHMODE_FULLSCREEN);
         
@@ -28,7 +33,23 @@ public class MainActivity extends SlidingActivity {
         actionBar.setHomeButtonEnabled(true);
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	
+    	switch (item.getItemId()) {
+			case R.id.action_settings:
+			Toast.makeText(this, "Settings gedrückt", Toast.LENGTH_SHORT).show();
+			break;
+			case android.R.id.home:
+			toggle();
+			break;
+		
+			default:
+			super.onOptionsItemSelected(item);
+			break;
+		}
+    	return super.onOptionsItemSelected(item);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
