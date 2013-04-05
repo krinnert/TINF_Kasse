89ee61dcc609a4e08fcdbb691640faaa014d6fcb
krinnert <kevin.rinnert@gmail.com>
2013-04-05 12:58:19 +0200

    Hoch mit der neuen Main-Activity

---
+++ MainActivity/src/com/example/smartkasse/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.smartkasse;
+
+import android.os.Bundle;
+
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.view.Menu;
+import com.slidingmenu.lib.SlidingMenu;
+import com.slidingmenu.lib.app.SlidingActivity;
+//changed by KR
+public class MainActivity extends SlidingActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setBehindContentView(R.layout.behind_layout);
+        
+        SlidingMenu slidingMenu = getSlidingMenu();
+        ActionBar actionBar = getSupportActionBar();
+        
+        //SlidingMenu einrichten
+        slidingMenu.setBehindOffset(150);
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        slidingMenu.setTouchModeBehind(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        
+        //ActionBar einrichten
+        actionBar.setHomeButtonEnabled(true);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getSupportMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
